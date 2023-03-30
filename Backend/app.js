fe962bedd8dbc33412f15aa6b26108168f9aaaa4
Man03 <107932536+Man03@users.noreply.github.com>
2023-03-30 15:13:46 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -24,12 +24,8 @@ app.use(
     saveUninitialized: false,
   })
 );
-const corsOptions = {
-  origin: "https://lost-found-ddu.netlify.app/",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+const cors = require('cors');
+app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 connectDB();
